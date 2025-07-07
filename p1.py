e0ed2f5184c7ca52629ad1eac33ceef5bf1e1b1a
Ankita Sharma <ankitasharma3143@gmail.com>
2025-07-07 23:19:05 +0530

    project1

---
+++ p1.py
@@ -0,0 +1,11 @@
+name = "ankita is smart"
+
+print(len(name))
+print(name.endswith("ta"))
+print(name.startswith("an"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.count("a"))
+print(name.find("smart"))
+print(name.replace("smart","paglu"))
