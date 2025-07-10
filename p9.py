a61a671e6690608205037893a3941fe2e7d522fc
Ankita Sharma <ankitasharma3143@gmail.com>
2025-07-11 00:59:56 +0530

    project

--- p9.py
+++ p9.py
@@ -1,7 +1,10 @@
-a, b, c = map(int, input("Enter 3 numbers: ").split())
+a= int(input("enter a ="))
+b= int(input("enter b ="))
+c= int(input("enter c ="))
 if a>=b and a>=c:
-    print("a is the largest number")
+   print("a is the largest number")
 elif b>=a and b>=c:
-    print("b is the largest")
+   print("b is the largest")
 else:
-    print("c is the largest")        
+   print("c is the largest")        
+
